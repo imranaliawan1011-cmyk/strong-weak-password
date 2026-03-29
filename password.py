9d03866ba9e59f08f9542b0a64cc75ea02207ded
Imran Ali Awan <imranaliawan1011@gmail.com>
2026-03-29 17:15:24 +0500

    first-commit

---
+++ password.py
@@ -0,0 +1,11 @@
+import re
+
+password = input("Enter password: ")
+
+if (len(password) >= 8 and
+    re.search(r"[a-z]", password) and
+    re.search(r"[0-9]", password) and
+    re.search(r"[@#$%&*!]", password)):
+    print("Strong Password ")
+else:
+    print("Weak Password ")
